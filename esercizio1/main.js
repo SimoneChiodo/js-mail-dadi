071e6ec760ea8f1642a273f2e458910f1d23f891
SimoneChiodo <simochi.005@gmail.com>
2024-10-17 13:23:54 +0200

    esercizio 1

--- esercizio1/main.js
+++ esercizio1/main.js
@@ -2,3 +2,16 @@
 // Chiedi all'utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull'esito del controllo.
+
+// Elenco delle email salvate
+const emails = ["pippo@email.it", "pluto@email.it", "paperino@email.it"];
+
+// Richiesta della email dell'utente
+const userEmail = prompt("Inserisci la tua email: ");
+
+// Mostro all'utente se la mail è salvata
+alert(
+    emails.includes(userEmail)
+        ? `Sei già registrato`
+        : `Devi effetuare la registrazione`
+);
