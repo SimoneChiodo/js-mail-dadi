2a3417a3970804ce0afacb2bd42aa41b2f993edb
SimoneChiodo <simochi.005@gmail.com>
2024-10-17 13:43:32 +0200

    esercizio 2

--- esercizio2/main.js
+++ esercizio2/main.js
@@ -1,3 +1,19 @@
 // Testo:
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Genero il numero dell'utente e del computer
+let userNumber = Math.floor(Math.random() * 6) + 1,
+    computerNumber = Math.floor(Math.random() * 6) + 1;
+
+// Informo l'utente dei numeri generati
+alert(
+    `Il tuo numero è: ${userNumber}. \nIl numero del computer è: ${computerNumber}.`
+);
+
+// Informo l'utente del vincitore
+alert(
+    userNumber > computerNumber
+        ? `HAI VINTO!🎉 \n(☞ﾟヮﾟ)☞`
+        : `Hai perso!😑 \nಥ_ಥ`
+);
